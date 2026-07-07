d6817ede8894dbfedaabe374b57004ba85270446
sylbardau <bardau.sylvain@gmail.com>
2026-07-07 09:16:06 +0000

    fix: terraform fmt

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ecf-infoline-965932218164" # même nom que dans bootstrap-backend.sh
-    key            = "prod/terraform.tfstate"
-    region         = "eu-west-3"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "ecf-infoline-965932218164"
+    key          = "prod/terraform.tfstate"
+    region       = "eu-west-3"
+    use_lockfile = true
+    encrypt      = true
   }
 }
