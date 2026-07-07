e89e2ac5c479881afcf935b6c366497ee91ff7fa
sylbardau <bardau.sylvain@gmail.com>
2026-07-07 20:53:38 +0000

    modification du ci/cd

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "ecf-infoline-965932218164" # même nom que dans bootstrap-backend.sh
     key            = "prod/terraform.tfstate"
     region         = "eu-west-3"
-    dynamodb_table = "terraform-locks"
+    use_lockfile = true
     encrypt        = true
   }
 }
